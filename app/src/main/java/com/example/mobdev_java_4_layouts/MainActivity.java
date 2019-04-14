cdc2a1b365ca143994e52150695b03fc2b8186ae
Denis Panin <keytaro721@hotmail.com>
2019-04-14 17:19:22 +0300

    Task 2 LinearLayout

--- app/src/main/java/com/example/mobdev_java_4_layouts/MainActivity.java
+++ app/src/main/java/com/example/mobdev_java_4_layouts/MainActivity.java
@@ -11,15 +11,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.myscreen);
-
-//        LinearLayout llBottom = (LinearLayout) findViewById(R.id.llBottom);
-//        TextView tvBottom = (TextView) findViewById(R.id.tvBottom);
-//        Button btnBottom = (Button) findViewById(R.id.btnBottom);
-//
-//        llBottom.setBackgroundResource(R.color.llBottomColor);
-//        tvBottom.setText(R.string.tvBottomText);
-//        btnBottom.setText(R.string.btnBottomText);
-
+        setContentView(R.layout.activity_main);
     }
 }
